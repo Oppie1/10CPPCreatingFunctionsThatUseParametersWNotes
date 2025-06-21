c2c1f646e6d350d3987023339f0b4efb023f0a6a
Oppie1 <atindall729@gmail.com>
2025-06-21 16:22:45 -0500

    Update FileName.cpp

--- 10CPPCreatingFunctionsThatUseParametersWNotes/10CPPCreatingFunctionsThatUseParametersWNotes/FileName.cpp
+++ 10CPPCreatingFunctionsThatUseParametersWNotes/10CPPCreatingFunctionsThatUseParametersWNotes/FileName.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 
-void printCrap(int x) { //Whever we use a parapeter we put the information here. It is looking for int x
+void printCrap(int x) { //We use void becuase this does not return anything. It gets sent to main and that is it.
+	//Main is where the program is run from in this "void" Situation
+	//Whever we use a parameter we put the information here. It is looking for int x
 	//It needs one integer in order to work.  Remember top down
 	cout << "buckys favortie number is " << x << endl; //This function is called fully from top
-	//It is not a protorype.  It is providing the sentence. In main when 
+	//Nothing is returned and the program is not "run" from a return value here. Nothing comes back 
 }
 
 int main()
@@ -14,4 +16,7 @@ int main()
 	return 0;
 }
 
-//Whenever making parameters you need to list what type of data and variable kname (int and x)
+//Whenever making parameters you need to list what type of data and variable name (int and x)
+//So in this situation main gets the sentence from void printCrap and prints it to the screen
+//Without void means it returns something back to main. Main asks for it, it does a calculation and
+//retuns its own value.
